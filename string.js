0b3d19d9677352f5f219518b11d7bb548666fa83
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-21 18:59:14 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,38 @@
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.length}`)
+// console.log= (`Before :  ${txt.toLocaleLowerCase()}`)
+// console.log= (`Before :  ${txt.toLocaleUpperCase()}`)
+
+
+
+// *************Include*************** <Find If The Data Has The Word Or Numbers In It>
+
+
+// // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.includes("publishing")}`)
+
+
+// **************StartWith**************
+
+
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.with(" graphic")}`)
+
+
+// ************EndsWith***********
+
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.endsWith("demonstrate")}`)
+
+
+
+// **********Search***********
+
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.search("and")}`)
+
+
+// ********Match*********
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log= (`Before :  ${txt.match("In")}`)
+
