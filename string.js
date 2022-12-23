7545f3b6e1227f340aa6afa93dcba13e5fbbbb2d
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-23 17:53:21 +0500

    Add files via upload

--- string.js
+++ string.js
@@ -1,7 +1,7 @@
 // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.length}`)
-// console.log= (`Before :  ${txt.toLocaleLowerCase()}`)
-// console.log= (`Before :  ${txt.toLocaleUpperCase()}`)
+// console.log (`Before :  ${txt.length}`)
+// console.log (`Before :  ${txt.toLocaleLowerCase()}`)
+// console.log(`Before :  ${txt.toLocaleUpperCase()}`)
 
 
 
@@ -9,30 +9,110 @@
 
 
 // // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.includes("publishing")}`)
+// console.log (`Before :  ${txt.includes("publishing")}`)
 
 
 // **************StartWith**************
 
 
 // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.with(" graphic")}`)
+// console.log (`Before :  ${txt.with(" graphic")}`)
 
 
 // ************EndsWith***********
 
 // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.endsWith("demonstrate")}`)
+// console.log (`Before :  ${txt.endsWith("demonstrate")}`)
 
 
 
 // **********Search***********
 
 // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.search("and")}`)
+// console.log (`Before :  ${txt.search("and")}`)
 
 
 // ********Match*********
 // let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
-// console.log= (`Before :  ${txt.match("In")}`)
+// console.log(`Before :  ${txt.match("In")}`)
+
+// **********IndexOf*************
+
+// let txt = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate"
+// console.log(`Before :  ${txt.indexOf("Lorem")}`)
+
+
+// *************LastIndexOf************
+
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.lastIndexOf("is")}`)
+
+
+// ***********rePlace***********
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// // console.log(`Before :  ${txt.replace("s","F")}`)
+// console.log(`Before :  ${txt.replace(/ip/g,"s",)}`)
+
+
+// **********Trim*********** *Console pr nhi chle ga*
+
+
+// let txt = "                  Lorem, ipsum           dolor sit amet consectetur adipisicing elit."
+// document.write(`Before :  ${txt.trim()}`)
+
+
+// **********charAt*********
+
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.charAt(7)}`)
+
+// // ******charCodeAt********
+
+
+// let txt1 = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt1.charCodeAt("p")}`)
+
+// ********fromCharCode**********
+
+// let a = String.fromCharCode(69);
+// console.log(a)
+
+// *********conCat*********
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.concat("Hello","World",78," ")}`)
+
+
+
+// ******Split********
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.split("FM")}`)
+
+
+// *******Repeat*******
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.repeat(5)}`)
+
+
+
+
+// ********Slice***********
+
+// let txt = "Lorem,ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.slice(4,9)}`)
+
+
+// *******subStr********
+
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.substr(7,10)}`)
+
+// ******subString******** <better than slice  "ulta kaam krskta hai">
+// let txt = "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+// console.log(`Before :  ${txt.substring(7,10)}`)
 
